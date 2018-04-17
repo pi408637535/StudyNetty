cebc17ee8eaf447e0a9917a704e67ca03ea1525f
galy <piguanghua@wordemotion.com>
2018-04-17 16:48:08 +0800

    实现粘包的模拟bug

---
+++ src/main/java/com/study/netty/sticky/client/NettyStickyTimeClient.java
@@ -0,0 +1,54 @@
+package com.study.netty.sticky.client;
+
+import com.study.nio.basic.client.NettyTimeClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by piguanghua on 4/17/18.
+ */
+public class NettyStickyTimeClient {
+    public void connect(String host,int port)throws Exception{
+        // 配置服务端的NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            // Bootstrap 类，是启动NIO服务器的辅助启动类
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                                throws Exception{
+                            ch.pipeline().addLast(new NettyStickyTimeClientHandler());
+                        }
+                    });
+
+            // 发起异步连接操作
+            ChannelFuture f= b.connect(host,port).sync();
+
+            // 等待客服端链路关闭
+            f.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[]args)throws Exception{
+        int port = 8080;
+        if(args!=null && args.length>0){
+            try {
+                port = Integer.valueOf(args[0]);
+            }
+            catch (NumberFormatException ex){}
+        }
+        new NettyStickyTimeClient().connect("127.0.0.1",port);
+    }
+}
---
+++ src/main/java/com/study/netty/sticky/server/NettyStickyTimeServerHandler.java
@@ -0,0 +1,53 @@
+package com.study.netty.sticky.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+/**
+ * Created by piguanghua on 4/17/18.
+ */
+public class NettyStickyTimeServerHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    // 用于网络的读写操作
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "utf-8").substring(0, req.length - System.getProperty("line.separator").length());
+        System.out.println("The time server receiver order:" + body + "; the counter is:" + ++counter);
+
+
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(
+                System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime += System.getProperty("line.separator");   // System.getProperty("line.separator")，获取/n的作用
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+
+        // 当客户端和服务端建立tcp成功之后，Netty的NIO线程会调用channelActive
+        // 发送查询时间的指令给服务端。
+        // 调用ChannelHandlerContext的writeAndFlush方法，将请求消息发送给服务端
+        // 当服务端应答时，channelRead方法被调用
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();   // 它的作用是把消息发送队列中的消息写入SocketChannel中发送给对方
+        // 为了防止频繁的唤醒Selector进行消息发送，Netty的write方法，并不直接将消息写入SocketChannel中
+        // 调用write方法只是把待发送的消息发到缓冲区中，再调用flush，将发送缓冲区中的消息
+        // 全部写到SocketChannel中。
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
